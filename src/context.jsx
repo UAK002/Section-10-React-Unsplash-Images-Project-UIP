546e53a76c3679a873c2f9775607ccfca6fc37b0
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-08-02 08:33:36 +0530

    322. Dark Theme - Initial Setup

--- src/context.jsx
+++ src/context.jsx
@@ -3,11 +3,17 @@ import { createContext, useContext, useState, useEffect } from 'react';
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  // const greeting = 'hello';
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+  };
 
   return (
-    // <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
-    <AppContext.Provider value={{}}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
