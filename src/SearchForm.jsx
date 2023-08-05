963304badbb46d2974402cff7db0171b94f366b9
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-08-05 08:29:32 +0530

    325. SearchForm

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,4 +1,28 @@
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          placeholder="cat"
+          className="form-input search-input"
+        />
+        <button type="submit" className="btn">
+          search
+          https://api.unsplash.com/search/photos?client_id=ZpHERRZLFMcptAlGTYga5y0P3lz4hP1slFkQXQeq9Wo&
+          query=office
+        </button>
+      </form>
+    </section>
+  );
 };
 export default SearchForm;
