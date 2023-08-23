319e1e2463db2fde7f2e1c77026c005e90cfb075
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-08-24 03:10:52 +0530

    338. ENV Variables

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -4,7 +4,11 @@ import { useGlobalContext } from './context';
 
 const url =
   // 'https://api.unsplash.com/search/photos?client_id=ZpHERRZLFMcptAlGTYga5y0P3lz4hP1slFkQXQeq9Wo&query=cat';
-  'https://api.unsplash.com/search/photos?client_id=ZpHERRZLFMcptAlGTYga5y0P3lz4hP1slFkQXQeq9Wo';
+  // 'https://api.unsplash.com/search/photos?client_id=ZpHERRZLFMcptAlGTYga5y0P3lz4hP1slFkQXQeq9Wo';
+  `https://api.unsplash.com/search/photos?client_id=${
+    import.meta.env.VITE_API_KEY
+  }`;
+console.log(import.meta.env.VITE_API_KEY);
 
 const Gallery = () => {
   const { searchTerm } = useGlobalContext();
